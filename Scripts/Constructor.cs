20fae525fab8e9679eef119c5cc6103d851829c9
ZapilinEntertainment <e.zapilin@gmail.com>
2021-04-16 18:29:35 +0500

    Start update

---
+++ Scripts/Constructor.cs
@@ -0,0 +1,173 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class Constructor : MonoBehaviour
+{
+    [SerializeField] private GameObject example, controlPanel, selectorFrame;
+    [SerializeField] private Transform deckHoster;
+    
+    private float[] basicHexesValues = new float[6];
+    private float outerRadius, innerRadius;
+    private Dictionary<HexPosition, Hex> hexList;
+    private Hex selectedHex;
+    private const float CONST_0 =1.73205f; 
+
+    private GameObject CreateNewHexPlace(HexPosition hpos)
+    {
+        var g = Instantiate(example, example.transform.position, example.transform.rotation, deckHoster);
+        g.transform.position += GetHexOnCanvasPosition(hpos);        
+        g.name = "hex " + hpos.ringIndex.ToString() + ':' + hpos.ringPosition.ToString();
+        g.SetActive(true);
+        var hex = new Hex(hpos, g.GetComponent<Image>());
+        hexList.Add(hpos, hex);            
+        return g;
+    }
+    private Hex CreateNewHex(HexPosition hpos, ColorCode cc, HexSpriteType hst)
+    {
+        var g = Instantiate(example, example.transform.position, example.transform.rotation, deckHoster);
+        g.transform.position += GetHexOnCanvasPosition(hpos);
+        g.name = "hex " + hpos.ringIndex.ToString() + ':' + hpos.ringPosition.ToString();
+        g.SetActive(true);
+        var hex = new Hex(hpos, g.GetComponent<Image>(), cc, hst);
+        hexList.Add(hpos, hex);
+        return hex;
+    }
+    private Vector3 GetHexOnCanvasPosition(HexPosition hpos)
+    {
+        switch (hpos.ringIndex)
+        {
+            case 0: return GetMainDirectionVector(hpos.ringPosition) * 2f * innerRadius;
+                /*
+            case 1:
+                float s;
+                if (hpos.ringPosition % 2 == 1) s = 4f * innerRadius;
+                else s = 2f * outerRadius;
+                return (Quaternion.AngleAxis(30f * hpos.ringPosition, Vector3.forward) * Vector3.up) * s;
+                */
+            default:
+                {
+                    byte sector = hpos.DefineSector();                    
+                    var dir = GetMainDirectionVector(sector) * (innerRadius * 2f * (hpos.ringIndex + 1));
+                    dir += GetInsectorDirection(sector)* hpos.DefineInsectorPosition() * 2f * innerRadius;
+                    return dir;
+                }
+        }
+    }
+    private Vector3 GetMainDirectionVector(byte x)
+    {
+        return (Quaternion.AngleAxis(60f * x, Vector3.back) * Vector3.up);
+    } 
+    private Vector3 GetInsectorDirection(byte x)
+    {
+        // normalized
+        switch (x)
+        {
+            case 0: return new Vector3(CONST_0, -1f , 0f).normalized;
+            case 1: return Vector3.down ;
+            case 2: return new Vector3(-0.9f, -0.4f , 0f);
+            case 3: return new Vector3(-0.9f, 0.4f, 0f);
+            case 4: return Vector3.up;
+            case 5: return new Vector3(0.9f, 0.4f, 0f);
+            default:return Vector3.zero;
+        }
+    }
+
+
+    private void Start()
+    {
+        hexList = new Dictionary<HexPosition, Hex>();
+        innerRadius = example.GetComponent<RectTransform>().rect.width * 0.4f;
+        outerRadius = innerRadius * 2f / CONST_0;
+
+        HexPosition hpos = new HexPosition(0, 0);
+        CreateNewHex(hpos, ColorCode.Red, HexSpriteType.Fixed);
+        hpos = new HexPosition(0, 1);
+        CreateNewHex(hpos, ColorCode.Blue, HexSpriteType.Fixed);
+        hpos = new HexPosition(0, 2);
+        CreateNewHex(hpos, ColorCode.Green, HexSpriteType.Fixed);
+        hpos = new HexPosition(0, 3);
+        CreateNewHex(hpos, ColorCode.Green, HexSpriteType.Fixed);
+        hpos = new HexPosition(0, 4);
+        CreateNewHex(hpos, ColorCode.Blue, HexSpriteType.Fixed);
+        hpos = new HexPosition(0, 5);
+        CreateNewHex(hpos, ColorCode.Red, HexSpriteType.Fixed);
+
+        GameObject g;
+        Button b;
+        for (int i =0; i < 12; i++)
+        {
+            var pos = new HexPosition(1, i);
+            g = CreateNewHexPlace(pos);
+            b = g.GetComponent<Button>();          
+            b.onClick.AddListener (() => this.HexClicked(pos));
+            b.enabled = true;
+        }
+        return;
+        for (int i = 0; i < 3; i++)
+        {
+            var pos = new HexPosition(2, i);
+            g = CreateNewHexPlace(pos);
+            b = g.GetComponent<Button>();
+            b.onClick.AddListener(() => this.HexClicked(pos));
+            b.enabled = true;
+        }
+    }
+
+   
+
+    public void HexClicked(HexPosition hpos)
+    {
+        if (hexList.TryGetValue(hpos, out selectedHex))
+        {
+            controlPanel.SetActive(true);
+            selectorFrame.transform.position = selectedHex.position;
+            selectorFrame.transform.SetAsLastSibling();
+            selectorFrame.SetActive(true);
+        }
+        else
+        {
+            if (selectorFrame.activeSelf)
+            {
+                selectorFrame.SetActive(false);
+                controlPanel.SetActive(false);
+            }
+        }
+    }
+
+    public void ControlPanel_SetColor(int i)
+    {
+        if (selectedHex != null)
+        {
+            ColorCode cc = ColorCode.White;
+            if (i == 0) cc = ColorCode.Red;
+            else
+            {
+                if (i == 1) cc = ColorCode.Green;
+                else cc = ColorCode.Blue;
+            }
+            selectedHex.SetColor(cc);
+            CreateNext(selectedHex.hexPosition);
+            selectedHex = null;
+        }
+        controlPanel.SetActive(false);
+        selectorFrame.SetActive(false);
+    }
+    
+
+    private void CreateNext(HexPosition i_hp)
+    {
+        HexPosition hpos = HexPosition.zer0;
+        switch (i_hp.ringIndex)
+        {
+            case 1:
+                if (i_hp.ringPosition % 2 == 1) hpos = new HexPosition(i_hp.ringPosition, i_hp.ringPosition + 1);
+                else hpos = new HexPosition(i_hp.ringPosition + 1, (i_hp.ringPosition -1) /2 * 3);
+                break;
+        }
+        CreateNewHexPlace(hpos);
+    }
+}
---
+++ Scripts/Hex.cs
@@ -0,0 +1,144 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public enum ColorCode : byte { White, Red, Green, Blue }
+public enum HexSpriteType : byte { Normal, Fixed, Available, Frame }
+public struct HexPosition
+{
+    public byte ringIndex, ringPosition;
+    public static readonly HexPosition zer0 = new HexPosition(0, 0);
+
+    public HexPosition(byte index, byte position)
+    {
+        ringIndex = index;
+        ringPosition = position;
+    }
+    public HexPosition(int index, int position)
+    {
+        if (index < 0 || index > 255) { index = 255; Debug.Log("error in hexpositon index"); }
+        if (position < 0 || position > 255) { position = 255; Debug.Log("error in hexposition pos"); }
+        ringIndex = (byte)index;
+        ringPosition = (byte)position;
+    }
+    public byte DefineSector()
+    {
+        if (ringIndex == 0) return ringPosition;
+        else return (byte)(ringPosition / (ringIndex + 1));
+    }
+    public byte DefineInsectorPosition()
+    {
+        if (ringIndex == 0) return ringPosition;
+        else {
+            int x = ringIndex + 1;
+            int a = ringPosition / x;
+            if (ringPosition % x == 0) return 0;
+            else return (byte)(ringPosition - a * x);
+        }
+    }
+}
+
+public class Hex
+{
+    public override bool Equals(object obj)
+    {
+        // Check for null values and compare run-time types.
+        if (obj == null || GetType() != obj.GetType())
+            return false;
+        else return true;
+    }
+    public static bool operator ==(Hex A, Hex B)
+    {
+        if (ReferenceEquals(A, null))
+        {
+            return ReferenceEquals(B, null);
+        }
+        return A.Equals(B);
+    }
+    public static bool operator !=(Hex A, Hex B)
+    {
+        return !(A == B);
+    }
+
+    public ColorCode colorcode { get; private set; }
+    private HexSpriteType _spriteType;
+    public HexSpriteType spriteType { get { return _spriteType; }
+        private set {
+            if (image != null)
+                _spriteType = value;
+                image.sprite = GetHexSprite(value);
+            }
+    }
+    public Vector3 position { get { return image?.transform.position ?? Vector3.zero; } }
+    public HexPosition hexPosition { get; private set; }
+    private Image image;
+
+    private static readonly Color redColor = Color.Lerp(Color.red, Color.white, 0.7f),
+        blueColor = Color.Lerp(Color.blue, Color.white, 0.7f),
+        greenColor = Color.Lerp(Color.green, Color.white, 0.7f);
+    private static readonly Sprite[] hexSprites;
+    public static Sprite GetHexSprite(HexSpriteType hst)
+    {
+        switch (hst)
+        {
+            case HexSpriteType.Fixed: return hexSprites[0];
+            case HexSpriteType.Available: return hexSprites[1];            
+            case HexSpriteType.Frame: return hexSprites[2];
+            default: return hexSprites[3];
+        }
+    }
+    static Hex() {
+        hexSprites = Resources.LoadAll<Sprite>("hex");
+     }
+    public static Color GetColorByColorcode(ColorCode cc)
+    {
+        switch (cc)
+        {
+            case ColorCode.Red: return redColor;
+            case ColorCode.Blue: return blueColor;
+            case ColorCode.Green: return greenColor;
+            default: return Color.white;
+        }
+    }
+
+    public Hex(HexPosition i_pos, Image i_img)
+    {
+        hexPosition = i_pos;
+        image = i_img;
+        colorcode = ColorCode.White;
+        spriteType = HexSpriteType.Available;        
+    }
+    public Hex(HexPosition i_pos, Image i_img, ColorCode i_clr)
+    {
+        hexPosition = i_pos;
+        image = i_img;
+        colorcode = i_clr;
+        spriteType = HexSpriteType.Normal;
+        image.color = GetColorByColorcode(colorcode);
+    }
+    public Hex(HexPosition i_pos, Image i_img, ColorCode i_clr, HexSpriteType hst) 
+    {
+        hexPosition = i_pos;
+        image = i_img;
+        colorcode = i_clr;
+        spriteType = hst;
+        image.color = GetColorByColorcode(colorcode);
+    }
+
+    public void SetColor(ColorCode cc)
+    {
+        colorcode = cc;
+        image.color = GetColorByColorcode(colorcode);
+        if (spriteType == HexSpriteType.Available)
+        {
+            // UNBLOCKING
+            spriteType = HexSpriteType.Normal;
+        }
+    }
+    public void SetImage(HexSpriteType hst)
+    {
+        spriteType = hst;
+        if (hst == HexSpriteType.Frame || hst == HexSpriteType.Available) colorcode = ColorCode.White;
+    }
+}
